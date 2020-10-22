eb4b1643b279ed5d5dcfac9c2bd07ac3adfd6eb6
motorailgun <28751910+motorailgun@users.noreply.github.com>
2020-10-22 17:38:46 +0900

    ocl: ahokusa typo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage_ocl.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocn-user \
-    lineage_ocn-userdebug \
-    lineage_ocn-eng
+    lineage_ocl-user \
+    lineage_ocl-userdebug \
+    lineage_ocl-eng
