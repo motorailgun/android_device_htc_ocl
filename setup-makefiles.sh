50ca71ba09a0945face64b76e36af5f6fab87f8e
motorailgun <28751910+motorailgun@users.noreply.github.com>
2020-10-23 07:08:28 +0900

    ocl: update extract-files.sh and setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -18,10 +18,10 @@
 
 set -e
 
-DEVICE=*** FILL IN DEVICE ****
-VENDOR=*** FILL IN VENDOR ****
+DEVICE=ocl
+VENDOR=htc
 
-INITIAL_COPYRIGHT_YEAR=**** FILL IN COPYRIGHT YEAR ****
+INITIAL_COPYRIGHT_YEAR=2020
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
